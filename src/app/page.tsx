841c77055a9974a1f4965f61f63c32879216b3d4
Komuta <yuri.komuta@gmail.com>
2025-03-10 03:32:09 -0300

    ajuste do acesso mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import logo from './assets/logo-LivroSaaS.svg';
 import womanImg from './assets/woman.svg';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Check } from 'lucide-react';
+import { Check, MenuIcon } from 'lucide-react';
 import {
   Card,
   CardContent,
@@ -17,21 +17,22 @@ import {
 export default function Home() {
   return (
     <main>
-      <section className=" container mx-auto text-center">
+      <section className=" container mx-auto text-center pb-20 px-4 md:px-0">
         <nav className="flex justify-between items-center py-4">
           <Image src={logo} alt="logo-LivroSaaS" />
-          <div className="flex items-center gap-2">
+          <MenuIcon size={20} className='md:hidden cursor-pointer'/>
+          <div className=" items-center gap-2 hidden md:flex">
             <Button variant={'link'}>Funcionamento</Button>
             <Button variant={'link'}>Preço</Button>
             <Button variant={"bg-white"}>Login</Button>
           </div>
         </nav>
-        <h1 className="text-6xl font-bold mt-16 pb-20">Simplifique Seus Estudos</h1>
-        <p className='text-gray-500 font-bold text-xl max-w-3xl mx-auto '>
+        <h1 className="md:text-6xl text-2xl font-bold mt-8 md:mt-16 ">Simplifique Seus Estudos</h1>
+        <p className='text-gray-500 font-bold text-sm md:text-xl max-w-3xl mx-auto mt-4 '>
           Deixe que que nós fazemos a curadoria para você. Assine nossa plataforma e receba todos os meses um ebook novo de programação.
         </p>
 
-        <form className='mt-16'>
+        <form className='md:mt-16 mt-10'>
           <div className='flex gap-2 justify-center '>
             <Input type="email" name="newsletter" placeholder="Coloque seu email" className='max-w-sm border-gray-300 border'/>
             <Button type="submit">Assine Agora</Button>
@@ -41,12 +42,12 @@ export default function Home() {
         </form>
       </section>
 
-      <section className="bg-white py-16">
+      <section className="bg-white md:py-16 py-8">
         <div className='container mx-auto'>
-        <h2 className='text-5xl font-bold text-center'>Como Funciona?</h2>
-        <div className='mx-24 flex items-center justify-between'>
-          <Image src={womanImg} alt="carregando livros" />
-          <ul className='text-2xl text-muted-foreground space-y-6 flex-shrink-0'>
+        <h2 className='text-2xl md:text-4xl font-bold text-center'>Como Funciona?</h2>
+        <div className='mx-24 xl:mx-80 flex flex-col md:flex-row items-center justify-between'>
+          <Image src={womanImg} alt="carregando livros" className='max-w-xs'/>
+          <ul className='md:text-2xl  text-muted-foreground space-y-4 md:space-y-6 flex-shrink-0'>
             <li className='flex items-center justify-between gap-4'>Acesso a 1 ebook por mês<Check size={24}className='text-green-600'/> </li>
             <li className='flex items-center justify-between' >Curadoria especial <Check size={24}className='text-green-600'/></li>
             <li className='flex items-center justify-between' >Cancele quando quiser <Check size={24}className='text-green-600'/></li>
@@ -55,11 +56,11 @@ export default function Home() {
         </div>
       </section>
 
-      <section className=" py-16 text-center">
-        <h2 className="text-5xl font-bold mt-16 pb-20">Preço Simples e Transparente</h2>
-        <p className='text-gray-500 text-xl max-w-3xl mx-auto '>Pra que inúmeros planos quando nós sabemos exatamente o que é melhor para você? Assine o nosso plano mensal Pro Premium VIP e garanta mensalmente um ebook novo de programação.É por menos de um café por dia.</p>
+      <section className=" py-16 text-center ">
+        <h2  className="md:text-6xl text-2xl font-bold mt-8 md:mt-16 ">Preço Simples e Transparente</h2>
+        <p className='text-gray-500 mt-4 text-sm md:text-xl max-w-3xl mx-auto px-2'>Pra que inúmeros planos quando nós sabemos exatamente o que é melhor para você? Assine o nosso plano mensal Pro Premium VIP e garanta mensalmente um ebook novo de programação.É por menos de um café por dia.</p>
 
-      <Card className='w-[350] mx-auto text-left mt-20'>
+      <Card className='w-[350] mx-auto text-left md:mt-20 mt-10 px-4'>
         <CardHeader>
           <CardTitle>Plano Pro Premium VIP</CardTitle>
           <CardDescription>Tudo que você precisa para seus estudos</CardDescription>
@@ -80,9 +81,9 @@ export default function Home() {
 
       </section>
 
-      <section className="py-16 text-center">
-        <h2 className="text-5xl font-bold mt-16 pb-20">Pronto Para Mudar a Sua Vida?</h2>
-        <p className='text-gray-500 text-xl max-w-3xl mx-auto '>Faça como milhares de outras pessoas. Assine nosso produto e tenha garantido seus estudos</p>
+      <section className="md:py-16 py-8 text-center">
+        <h2 className=" text-2xl font-bold mt-16 pb-20 ">Pronto Para Mudar a Sua Vida?</h2>
+        <p className='text-gray-500 text-xl max-w-3xl mx-auto px-2'>Faça como milhares de outras pessoas. Assine nosso produto e tenha garantido seus estudos</p>
         <Button type="submit" className='mt-14 mx-auto w-96'>Assine Agora</Button>
         <p className='text-xs text-muted-foreground mt-2'>Comece sua assinatura agora mesmo. Cancele quando quiser.</p>
 
